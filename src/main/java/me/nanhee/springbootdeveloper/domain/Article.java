d9c98802cc40a3015c1d0b7a3d6527a9f5bdd861
Eveieve <jhkdev1@gmail.com>
2025-03-27 19:50:33 +0900

    Create Entity without Lombok

---
+++ src/main/java/me/nanhee/springbootdeveloper/domain/Article.java
@@ -0,0 +1,49 @@
+package me.nanhee.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+public class Article {
+
+    @Id // ID필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // title 이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) { // 빌더 패턴으로 객체 생성 - 객체를 유연하고 직관적으로 생성하기 위해.
+        this.title = title;
+        this.content = content;
+    }
+
+    protected Article() { // base constructor
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
+// 빌더 패턴을 사용하면 어느 필드에 어느 값이 매칭되는지 쉽게 확인 가능.
+//new Article("abc", "def");
+//
+//Article.builder()
+//.title("abc")
+//.content("def")
+//.build();
