80fda55e72d354606f1ec1656f5eb3973a9b5ad8
Eveieve <jhkdev1@gmail.com>
2025-03-30 12:55:40 +0900

    feat: 테스트 코드 추가 완ㅇ료

---
+++ src/main/java/me/nanhee/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.nanhee.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
