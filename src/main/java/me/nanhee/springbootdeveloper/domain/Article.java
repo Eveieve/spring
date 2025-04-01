27a15f2732ce89debb821446da92ac6e78ba799a
Eveieve <jhkdev1@gmail.com>
2025-04-01 09:11:50 +0900

    fix: table 만들어지지 않아 데이터 들어가지 않음 & 스프링부트 실행되지 않는 문제 해결

--- src/main/java/me/nanhee/springbootdeveloper/domain/Article.java
+++ src/main/java/me/nanhee/springbootdeveloper/domain/Article.java
@@ -1,5 +1,6 @@
 package me.nanhee.springbootdeveloper.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,6 @@
-create database blog;
-CREATE TABLE article (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         title VARCHAR(255),
-                         content TEXT
-);
 
-INSERT INTO article (title, content) values ('title 1', 'content 1')
-INSERT INTO article (title, content) VALUES ('title2', 'content2')
-INSERT INTO article (title, content) VALUES ('title3', 'content3')
+
+INSERT INTO Article (title, content) values ('title 1', 'content 1')
+INSERT INTO Article (title, content) VALUES ('title2', 'content2')
+INSERT INTO Article (title, content) VALUES ('title3', 'content3')
 
